c85df6e89d2b7fd7971931f8fda69101a20ece92
nhatan20032 <119768785+nhatan20032@users.noreply.github.com>
2023-12-06 14:08:00 +0700

    new caroulsel

---
+++ lib/component/ranking/caroulsel_ranking.dart
@@ -0,0 +1,211 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class CaroulselRank extends StatefulWidget {
+  const CaroulselRank({super.key});
+
+  @override
+  State<CaroulselRank> createState() => _CaroulselRankState();
+}
+
+class _CaroulselRankState extends State<CaroulselRank> {
+  final CarouselController _carouselController = CarouselController();
+  int _current = 0;
+
+  final List<dynamic> _movies = [
+    {
+      'title': 'Spider-Man',
+      'image':
+          'https://m.media-amazon.com/images/I/71hvoqd-X3L._AC_SL1357_.jpg',
+      'description': 'Spider-Man'
+    },
+    {
+      'title': 'The Amazing Spiderman 2',
+      'image':
+          'https://wallpapers.moviemania.io/phone/movie/102382/bee9ae/the-amazing-spider-man-2-phone-wallpaper.jpg?w=820&h=1459',
+      'description': 'The Amazing Spider-Man 2'
+    },
+    {
+      'title': 'Spider-Man No Way Home',
+      'image':
+          'https://9mmwallpapers.com/wp-content/uploads/Spider-Man-No-Way-Home-iPhone-Wallpaper-4k.jpg',
+      'description': 'Spider-Man No Way Home'
+    }
+  ];
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Image.network(_movies[_current]['image'], fit: BoxFit.cover),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [
+                      Colors.grey.shade50.withOpacity(1),
+                      Colors.grey.shade50.withOpacity(1),
+                      Colors.grey.shade50.withOpacity(1),
+                      Colors.grey.shade50.withOpacity(1),
+                      Colors.grey.shade50.withOpacity(0.0),
+                      Colors.grey.shade50.withOpacity(0.0),
+                      Colors.grey.shade50.withOpacity(0.0),
+                      Colors.grey.shade50.withOpacity(0.0),
+                    ])),
+              ),
+            ),
+            Positioned(
+              bottom: 50,
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                    height: 500.0,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.70,
+                    enlargeCenterPage: true,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _current = index;
+                      });
+                    }),
+                carouselController: _carouselController,
+                items: _movies.map((movie) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 320,
+                                  margin: const EdgeInsets.only(top: 30),
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Image.network(movie['image'],
+                                      fit: BoxFit.cover),
+                                ),
+                                const SizedBox(height: 20),
+                                Text(
+                                  movie['title'],
+                                  style: const TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                // rating
+                                const SizedBox(height: 20),
+                                Text(
+                                  movie['description'],
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      color: Colors.grey.shade600),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 20),
+                                AnimatedOpacity(
+                                  duration: const Duration(milliseconds: 500),
+                                  opacity: _current == _movies.indexOf(movie)
+                                      ? 1.0
+                                      : 0.0,
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 20.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.yellow,
+                                              size: 20,
+                                            ),
+                                            const SizedBox(width: 5),
+                                            Text(
+                                              '8.7',
+                                              style: TextStyle(
+                                                  fontSize: 14.0,
+                                                  color: Colors.grey.shade600),
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.access_time,
+                                              color: Colors.grey.shade600,
+                                              size: 20,
+                                            ),
+                                            const SizedBox(width: 5),
+                                            Text(
+                                              '3h',
+                                              style: TextStyle(
+                                                  fontSize: 14.0,
+                                                  color: Colors.grey.shade600),
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.2,
+                                          child: Row(
+                                            children: [
+                                              Icon(
+                                                Icons.play_circle_filled,
+                                                color: Colors.grey.shade600,
+                                                size: 20,
+                                              ),
+                                              const SizedBox(width: 5),
+                                              Text(
+                                                'Watch',
+                                                style: TextStyle(
+                                                    fontSize: 14.0,
+                                                    color:
+                                                        Colors.grey.shade600),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ));
+                    },
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
